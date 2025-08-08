7410f0cccc8b9fb86ef4085c0e86f8b0d85fb7d2
ByteFlipper <byteflipper.business@gmail.com>
2025-08-08 23:49:36 +0200

    feat: базовая структура сайта (лендинг, блог, about, download, privacy, terms), макет, header/footer, MDX пост

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,8 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  // TODO: замените на реальный домен проекта
+  site: 'https://sectlauncher.app',
   vite: {
     plugins: [tailwindcss()]
   },
